adda14e2a722080623e63929600c71881bec7398
Ayaminn <ayamin.imai@gmail.com>
2016-01-04 16:56:09 +0900

    Initial Commit

---
+++ countup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countup
+//
+//  Created by Ayami_Imai on 2016/01/04.
+//  Copyright (c) 2016年 Ayami_Imai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
